7b5f478352859dbc74b2745e4de5ae440f1d037d
Karl Schoch <karl.schoch@gmail.com>
2025-06-20 15:21:52 -0400

    Adds playlist name to fully integrate all elements passed into playlist component

--- code_academy_full_stack/jamming/src/components/Playlist.jsx
+++ code_academy_full_stack/jamming/src/components/Playlist.jsx
@@ -4,7 +4,7 @@ import Tracklist from './Tracklist'
 function Playlist({ tracklist, name }) {
     return (
         <div style={{ background: '#f0f0f0', padding: '1rem', minHeight: '300px' }}>
-            <h2>{name} Playlist</h2>
+            <h2>Playlist: {name} </h2>
             <Tracklist list={tracklist} />
         </div>
     )
