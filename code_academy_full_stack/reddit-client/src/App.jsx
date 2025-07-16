e893b95b09adb846eb508be9443300fc5c5884a2
Karl Schoch <karl.schoch@gmail.com>
2025-07-16 06:50:47 -0400

    Begins planning for the layout of the posts and subreddits sections and how it should adapt to smaller screen sizes

--- code_academy_full_stack/reddit-client/src/App.jsx
+++ code_academy_full_stack/reddit-client/src/App.jsx
@@ -9,7 +9,14 @@ function App() {
   return (
     <>
       <div className='banner'>This is the banner</div>
-      <div className='content'>This is the body</div>
+      <div className='content'>
+        This is the body
+        Vision: Within here, I want the posts and the subreddits
+          - under normal screen width, keep them side by side
+          - for smaller screen sizes, I want the subreddit to slip between the banner and the main content
+        - Implementation
+          - 
+      </div>
       
     </>
   )
