d9e89d223817f8bf2fe6ee8db508302ee8e7c30f
Karl Schoch <karl.schoch@gmail.com>
2025-07-17 06:52:10 -0400

    Creates high level styling for the page as well as posts section and begins to work on the styling for the subreddits section

--- code_academy_full_stack/reddit-client/src/App.jsx
+++ code_academy_full_stack/reddit-client/src/App.jsx
@@ -10,12 +10,58 @@ function App() {
     <>
       <div className='banner'>This is the banner</div>
       <div className='content'>
-        This is the body
+        {/* This is the body
         Vision: Within here, I want the posts and the subreddits
           - under normal screen width, keep them side by side
           - for smaller screen sizes, I want the subreddit to slip between the banner and the main content
         - Implementation
-          - 
+          - content has an additional 50 padding when screen < 400px
+          - subreddit and posts are a grid
+            - larger screen: 80/20
+            - small screen: 100/0 
+        */}
+        <div className='subreddits-mobile'>These are the subreddits. Will be a dropdown to save space</div>
+        <div className='posts'>
+          <div className='post'>
+            Main body of the post that includes text, photo
+            <br />
+            Time and <span className='user'>user</span>
+            <hr />
+            <div className='comment-toggle'>Comment Toggle (defaults to false to not display comments)</div>
+            <div className='comment-list'>
+              <div className='comment'>
+                Comment's Time and <span className='user'>user</span>
+                <br />
+                Comment text
+              </div>
+              <div className='comment'>
+                Comment's Time and <span className='user'>user</span>
+                <br />
+                Comment text
+              </div>
+            </div>
+          </div>
+          <div className='post'>
+            Main body of the post that includes text, photo
+            <br />
+            Time and <span className='user'>user</span>
+            <hr />
+            <div className='comment-toggle'>Comment Toggle (defaults to false to not display comments)</div>
+            <div className='comment-list'>
+              <div className='comment'>
+                Comment's Time and <span className='user'>user</span>
+                <br />
+                Comment text
+              </div>
+              <div className='comment'>
+                Comment's Time and <span className='user'>user</span>
+                <br />
+                Comment text
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='subreddits'>These are the subreddits</div>
       </div>
       
     </>
