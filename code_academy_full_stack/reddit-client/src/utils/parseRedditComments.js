58c74e94f9e0674fe466677f5e811cf1c8012e26
Karl Schoch <karl.schoch@gmail.com>
2025-07-31 10:01:40 -0400

    Created testing infrastructure for parsing the reddit comments, including simplifying test's input JSON, and begins implementation function

--- code_academy_full_stack/reddit-client/src/utils/parseRedditComments.js
+++ code_academy_full_stack/reddit-client/src/utils/parseRedditComments.js
@@ -1,23 +1,11 @@
 export function parseRedditComments(redditJson) {
-    if (!redditJson?.data?.children) return []
+    if (!redditJson?.data?.children) return {}
 
-    return redditJson.data.children.map((child) => {
-        const post = child.data;
-        const timePostedFormat = new Date(post.created * 1000)
-        return {
-            id: post.id,
-            title: post.title,
-            text: post.selftext,
-            subreddit: post.subreddit_name_prefixed,
-            img: null,
-            timePosted: timePostedFormat.toLocaleString(),
-            metadata: {
-                user: post.author,
-                comments: [],
-                upVoteCt: post.ups,
-                downVoteCt: post.downs,
-                commentCt: post.num_comments
-            }
-        }
-    })
+    // Pull out the id of the post of the first element in the array
+    // if it is not a post (aka kind == t3), return {}
+
+    // For the second element in the array
+    // if it is not a comment (kind == t1), continue
+    // 
+
 }
--- code_academy_full_stack/reddit-client/src/utils/parseRedditComments.test.js
+++ code_academy_full_stack/reddit-client/src/utils/parseRedditComments.test.js
@@ -1,19 +1,26 @@
 import singlePageJSON from '../assets/api-data/sample-single-page-data.json'
+import { parseRedditComments } from './parseRedditComments'
 
 describe('parseRedditComments', () => {
 
-    it('Renders MainPage component', async () => {
+    it('Extract comments for a given post', async () => {
         // Setup
-        // 1. Import the 
+        // 1. Import the JSON
 
         // Exercise
+        const res = parseRedditComments(singlePageJSON)
+        // function needs to validate that child is kind == "t1"
 
         // Verify
-        // Need to pull out an object where you have
-        // 1. An ID to uniquely ID the posts to put them into the slice
-        // 2. An list of comments
-        // 3. The commene
-        // { id: [ {comment1}, {comment2}, ...] }
+        expect(res).toEqual({
+            '1m25a7v': [
+                {
+                    'timestamp': 1752752450,
+                    'user': "AutoModerator",
+                    'text': "It looks like this post is about Politics. Various methods of filtering out content relating to Politics can be found [here](https://www.reddit.com/r/pics/wiki/v2/resources/filter/politics).\n\n*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](/message/compose/?to=/r/pics) if you have any questions or concerns.*",
+                }
+            ]
+        })
 
         // Teardown
 
