cf3338c904ef4706d1973f0e86335bad8ba440df
Karl Schoch <karl.schoch@gmail.com>
2025-08-07 06:48:54 -0400

    Implements first phase of testing around grabbit text input and button

--- code_academy_full_stack/reddit-client/src/components/Banner/SearchBar.jsx
+++ code_academy_full_stack/reddit-client/src/components/Banner/SearchBar.jsx
@@ -1,9 +1,24 @@
 import React, {useEffect, useState} from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onSubmit }) => {
+    const [value, setValue] = useState('')
+
+    const handleChage = (e) => {
+        setValue(e.target.value);
+    }
+
     return (
         <div data-testid='search-box'>
-            This is the search bar
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={handleChage}
+                    placeholder='Search Reddit...'
+                    data-testid='search-input'
+                />
+                <button type="submit" data-testid="submit-btn">Submit</button>
+            </form>
         </div>
     )
 }
