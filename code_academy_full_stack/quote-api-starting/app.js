b935d36df9a5d2e0ed10795ddbfe675e4eb17ef1
Karl Schoch <karl.schoch@gmail.com>
2025-09-21 17:04:36 -0400

    Working through GET /api/quotes

--- code_academy_full_stack/quote-api-starting/app.js
+++ code_academy_full_stack/quote-api-starting/app.js
@@ -8,7 +8,29 @@ const { getRandomElement } = require('./utils');
 app.use(cors());
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
 
+  const quoteIndex = Math.floor(Math.random() * quotes.length);
+  res.send(quotes[quoteIndex]);
+
+})
+app.get('/api/quotes', (req, res, next) => {
+
+  const queryParams = req.query
+  console.log(queryParams);
+
+  // need to check if there is a person param passed in or if there no params
+  // If there are no params, return all the quotes
+  // If there is a person param, Check to see if the person exists
+    // If they exist, send back there results
+    // If not, send back an empty array
+  if (!req.params) {
+
+  } else {
+    console.log('No params');
+  }
+
+})
 
 // export app for use in main.js and for testing
 module.exports = {
